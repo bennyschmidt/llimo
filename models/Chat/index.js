3202388ccd82f5a2fe2593f342d4239a78b5318c
exactchange <benny@exactchange.network>
2024-04-15 21:53:30 -0700

    Wh-words, irregulars, prefaces, modal verbs

--- models/Chat/index.js
+++ models/Chat/index.js
@@ -11,6 +11,15 @@ const {
 } = require('../../utils');
 
 const MATCH_PUNCTUATION = new RegExp(/[.,\/#!$%?“”\^&\*;:{}=\_`~()]/g);
+const MATCH_ARTICLES = new RegExp(/the|an/g);
+const MATCH_WH_REGULAR = new RegExp(/WHO|WHAT|WHEN|HOW/g);
+const MATCH_WHERE = new RegExp(/WHERE/g);
+const MATCH_WHY = new RegExp(/WHY/g);
+const MATCH_MODAL_REGULAR = new RegExp(/IS|ARE|WAS|HAS|HAVE|HAD|MUST|MAY|MIGHT|WERE|WILL|SHALL|CAN|COULD|WOULD|SHOULD|OUGHT/g);
+const MATCH_MODAL_DOES = new RegExp(/DOES/g);
+const MATCH_MODAL_DID = new RegExp(/DID/g);
+const LOCATED = "located";
+const BECAUSE = "because";
 
 /**
  * Create a language model agent from a
@@ -87,18 +96,22 @@ module.exports = async ({
   };
 
   const getPositionalPhrase = query => {
-    const firstWord = query.split(' ')[0].trim();
+    const tokens = query.split(' ');
+    const firstWord = tokens[0].trim();
+    const secondWord = tokens[1]?.trim?.() || '';
     const partsOfSpeech = getPartsOfSpeech(query);
 
     if (!partsOfSpeech?.length) return;
 
-    let phrase = partsOfSpeech.find(({ pos }) => pos.match(/VB/))?.value;
+    let cursor = secondWord;
 
-    if (firstWord.toUpperCase().match('WHERE')) {
-      phrase += ` located`;
+    // If where, append "located"
+
+    if (MATCH_WHERE.test(firstWord.toUpperCase())) {
+      cursor += ` ${LOCATED}`;
     }
 
-    return phrase || '';
+    return cursor || '';
   };
 
   const getFocus = query => {
@@ -124,7 +137,7 @@ module.exports = async ({
     const leadingPhrase = sequence.slice(0, sequence.indexOf(token.trim()));
     const leadingWord = leadingPhrase.split(' ').filter(Boolean).pop();
 
-    if (leadingWord === 'a' || leadingWord.match(/the|an/)) {
+    if (leadingWord === 'a' || leadingWord.toUpperCase().match(MATCH_ARTICLES)) {
       token = `${leadingWord} ${token}`;
     }
 
@@ -132,123 +145,184 @@ module.exports = async ({
   };
 
   /**
+   * querify
+   * Convert a raw input to a query
+   */
+
+  const querify = input => {
+    const tokens = input.split(' ');
+    const query = input.replace(MATCH_PUNCTUATION, '');
+
+    let firstWord = tokens[0].trim();
+
+    if (
+      MATCH_MODAL_REGULAR.test(firstWord) ||
+      MATCH_MODAL_DOES.test(firstWord) ||
+      MATCH_MODAL_DID.test(firstWord)
+    ) {
+      return query;
+    }
+
+    if (
+      MATCH_WH_REGULAR.test(firstWord) ||
+      MATCH_WHERE.test(firstWord) ||
+      MATCH_WHY.test(firstWord)
+    ) {
+      // TODO: Store Wh-word for focus context
+      //       For now just return the whole query
+
+      return query;
+    }
+
+    if (
+      MATCH_WH_REGULAR.test(query) ||
+      MATCH_WHERE.test(query) ||
+      MATCH_WHY.test(query)
+    ) {
+      // TODO: Store preface for focus context
+      //       For now just remove it
+
+      const whWord = (
+        query.indexOf(MATCH_WH_REGULAR) ||
+        query.indexOf(MATCH_WHERE) ||
+        query.indexOf(MATCH_WHY)
+      );
+
+      if (!whWord) return false;
+
+      // TODO: If there's a noun in the preface,
+      //       Replace the positional word with the
+      //       noun for noun context
+
+      return querify(query.slice(whWord).trim());
+    }
+
+    return query;
+  };
+
+  /**
+   * transform
+   * Transform a prompt to an answer
+   */
+
+  const transform = query => {
+    if (!query) return;
+
+    const { getCompletions } = languageModel;
+
+    // Example query: "is New York considered a melting pot?"
+
+    const firstWord = query.split(' ')[0].trim();
+    const nounPhrase = getNounPhrase(query).trim();
+    const positionalPhrase = getPositionalPhrase(query).trim();
+
+    // Get the first subject of the sentence
+    // Example: "New York"
+
+    let result = nounPhrase;
+
+    // Transpose with prepositions and verbs
+    // Example: "New York is considered"
+
+    result += ` ${positionalPhrase}`;
+
+    // Get completions
+    // Example A: ["New York is considered to be the melting pot of the world."]
+    // Example B: ["New York is considered to be one of the most ethnically diverse cities in the United States."]
+
+    let completions, sample;
+
+    sample = getCompletions(result.trim())?.completions;
+    completions = sample && sample.filter(Boolean);
+
+    console.log('COMPLETE', result.trim(), '...');
+
+    // If none and not a location, fallback to just completing
+    // the noun phrase
+
+    if (!completions?.length && !firstWord.toUpperCase().match('WHERE')) {
+      sample = getCompletions(nounPhrase)?.completions;
+      completions = sample && sample.filter(Boolean);
+      result = result.replace(positionalPhrase, '').trim();
+      console.log('FALLBACK', nounPhrase, '...');
+    }
+
+    // If none and the query is just 1 word, fallback to completing
+    // the first word of the query
+
+    if (!completions?.length && firstWord.trim() === query.trim()) {
+      sample = getCompletions(firstWord)?.completions;
+      completions = sample && sample.filter(Boolean);
+      result = firstWord.trim();
+      console.log('FALLBACK', firstWord, '...');
+    }
+
+    const keyword = (getFocus(query) || '').trim();
+
+    let completion = '';
+
+    const matchedPhrases = completions.filter(phrase => keyword && phrase.match(keyword));
+    const isMatch = !!matchedPhrases?.length;
+
+    console.log('MATCH', keyword, isMatch);
+
+    if (isMatch) {
+      // Filter to keyword matches
+
+      completions = matchedPhrases;
+    }
+
+    // Pull a ranked completion from the top ${RANKING_BATCH_SIZE}
+
+    completion = completions[
+      (Math.random() * completions.length) << 0
+    ];
+
+    console.log(completions);
+
+    // Handle no data
+
+    if (!completion) {
+      return "I'm not able to respond to that coherently due to lack of training data.";
+    }
+
+    // If why, append "because"
+
+    if (MATCH_WHY.test(firstWord.toUpperCase())) {
+      result += ` ${keyword} ${BECAUSE}`;
+    }
+
+    // Append completion
+    // Example: "New York is considered to be the melting pot of the world."
+
+    result = result.trim() + ` ${completion}`;
+
+    return result;
+  };
+
+  /**
    * ask
    * Question/answer transformer
    */
 
-  const ask = query => {
-    if (!query) return;
+  const ask = input => {
+    if (!input) return;
 
-    const {
-      getCompletions
-    } = languageModel;
+    // Convert the user input to a query
 
-    // Transforms a query into a combined completion
+    const query = querify(input);
 
-    const transform = query => {
-      // Example query: "is New York considered a melting pot?"
+    if (!query) {
+      return "I can't respond to commands yet.";
+    }
 
-      const firstWord = query.split(' ')[0].trim();
-      const nounPhrase = getNounPhrase(query).trim();
-      const positionalPhrase = getPositionalPhrase(query).trim();
-
-      // Handle inference query
-
-      if (
-        firstWord.toUpperCase() === 'IF' ||
-        firstWord.toUpperCase() === 'WHEN' ||
-        !isNaN(firstWord)
-      ) {
-        return "I'm not yet able to infer logic.";
-      }
-
-      // Get the first subject of the sentence
-      // Example: "New York"
-
-      let result = nounPhrase;
-
-      // Transpose with prepositions and verbs
-      // Example: "New York is considered"
-
-      result += ` ${positionalPhrase}`;
-
-      // Get completions
-      // Example A: ["New York is considered to be the melting pot of the world."]
-      // Example B: ["New York is considered to be one of the most ethnically diverse cities in the United States."]
-
-      let completions, sample;
-
-      sample = getCompletions(result.trim())?.completions;
-      completions = sample && sample.filter(Boolean);
-
-      console.log('COMPLETE', result.trim(), '...');
-
-      // If none and not a location, fallback to just completing
-      // the noun phrase
-
-      if (!completions?.length && !firstWord.toUpperCase().match('WHERE')) {
-        sample = getCompletions(nounPhrase)?.completions;
-        completions = sample && sample.filter(Boolean);
-        result = result.replace(positionalPhrase, '').trim();
-        console.log('FALLBACK', nounPhrase, '...');
-      }
-
-      // If none and the query is just 1 word, fallback to completing
-      // the first word of the query
-
-      if (!completions?.length && firstWord.trim() === query.trim()) {
-        sample = getCompletions(firstWord)?.completions;
-        completions = sample && sample.filter(Boolean);
-        result = firstWord.trim();
-        console.log('FALLBACK', firstWord, '...');
-      }
-
-      const keyword = (getFocus(query) || '').trim();
-
-      let completion = '';
-
-      const matchedPhrases = completions.filter(phrase => keyword && phrase.match(keyword));
-      const isMatch = !!matchedPhrases?.length;
-
-      console.log('MATCH', keyword, isMatch);
-
-      if (isMatch) {
-        // Filter to keyword matches
-
-        completions = matchedPhrases;
-      }
-
-      // Pull a ranked completion from the top ${RANKING_BATCH_SIZE}
-
-      completion = completions[
-        (Math.random() * completions.length) << 0
-      ];
-
-      console.log(completions);
-
-      // Handle no data
-
-      if (!completion) {
-        return "I'm not able to respond to that coherently due to lack of training data.";
-      }
-
-      // Append completion
-      // Example: "New York is considered to be the melting pot of the world."
-
-      result = result.trim() + ` ${completion}`;
-
-      return result;
-    };
-
-    // transform the input to the correct prompt
-
-    query = query.replace(MATCH_PUNCTUATION, '');
+    // Transform the query to an answer
 
     const answer = transform(query);
 
     // Format and return the answer
 
-    return toSentenceCase(answer);
+    return answer && toSentenceCase(answer);
   };
 
   // Chat API (extends Language)
